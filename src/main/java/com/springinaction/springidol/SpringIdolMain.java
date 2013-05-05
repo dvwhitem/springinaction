c215ac58b1b54abaacdcc8884229cea97e7bac08
dv <dv@dv>
2013-05-05 12:27:39 +0300

    First commit

---
+++ src/main/java/com/springinaction/springidol/SpringIdolMain.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.springinaction.springidol;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author dv
+ */
+public class SpringIdolMain {
+
+    public static void main(String[] args) throws PerformanceException {
+        
+        ApplicationContext ctx = 
+                new ClassPathXmlApplicationContext("com/springinaction/springidol/spring-idol.xml");
+        
+       // Performer performer = (Performer) ctx.getBean("kenny2");
+       // Performer performer = (Performer) ctx.getBean("duke");
+       Performer performer = (Performer) ctx.getBean("kenny"); 
+       performer.perform();
+    }
+}
